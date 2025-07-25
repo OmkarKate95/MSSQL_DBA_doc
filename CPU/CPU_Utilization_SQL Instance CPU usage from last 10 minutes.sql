69a48561443c93cc81038aa69a9fba51c87a5c15
Omkar <127608288+OmkarKate95@users.noreply.github.com>
2025-07-25 22:38:56 +0530

    Update CPU_Utilization_SQL Instance CPU usage from last 10 minutes.sql

--- CPU/CPU_Utilization_SQL Instance CPU usage from last 10 minutes.sql
+++ CPU/CPU_Utilization_SQL Instance CPU usage from last 10 minutes.sql
@@ -13,6 +13,40 @@ FROM (SELECT record.value('(./Record/@id)[1]','int')AS record_id,
 		record.value('(./Record/SchedulerMonitorEvent/SystemHealth/SystemIdle)[1]','int')AS [SystemIdle],
 		record.value('(./Record/SchedulerMonitorEvent/SystemHealth/ProcessUtilization)[1]','int')AS [SQLProcessUtilization],
 		[timestamp] FROM (SELECT[timestamp], convert(xml, record) AS [record]
-					            FROM sys.dm_os_ring_buffers
-								WHERE ring_buffer_type =N'RING_BUFFER_SCHEDULER_MONITOR' AND record LIKE'%%')AS x )AS y
-								ORDER BY record_id DESC;
+				   FROM sys.dm_os_ring_buffers
+				   WHERE ring_buffer_type =N'RING_BUFFER_SCHEDULER_MONITOR' AND record LIKE'%%')AS x )AS y
+				   ORDER BY record_id DESC;
+
+
+If exists(
+			Select * from #CPU
+			where Total_CPU > 10
+		)
+	BEGIN
+		-- Send email alert
+			EXECUTE msdb.dbo.sp_send_dbmail
+			@profile_name = 'airportwala',
+			@recipients = 'omkarkate0007@gmail.com',
+			@body = 'CPU Utilization > 10%.',
+			@subject = 'High CPU Utilization';
+	END
+
+-- Loading Values from #CPU to DBA.dbo.CPU
+
+if exists(
+			Select * from DBA.sys.tables
+			where name = 'CPU_info'
+		)
+	BEGIN
+		-- Destination table avilable
+			Insert into DBA.dbo.CPU_info
+			Select * from #CPU
+	END
+
+else
+	BEGIN
+		-- Destination tabel not avilavle
+			Select * into DBA.dbo.CPU_info
+			from #CPU
+	END
+DROP table #CPU
