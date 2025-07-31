7fcc36fef9a978cbfd0cdfb1fe521abd9cd48aba
Omkar <127608288+OmkarKate95@users.noreply.github.com>
2025-07-31 16:51:35 +0530

    Update DBCC_Commands (1) (1).sql

--- DBCC_Commands (1) (1).sql
+++ DBCC_Commands (1) (1).sql
@@ -20,8 +20,8 @@
 | DBCC indexdefrag		|-- Defragments clustered and secondary indexes (deprecated).		     |
 | DBCC inputbuffer		|-- Returns the last statement sent from a client to SQL Server.	             |
 | DBCC opentran			|-- Displays information about the oldest open transaction.	             |
-| DBCC outputbuffer		|-- Displays the output buffer of a specified SPID.			    |
-| DBCC pintable			|-- Pins a table in memory (deprecated).					    |
+| DBCC outputbuffer		|-- Displays the output buffer of a specified SPID.			     |
+| DBCC pintable			|-- Pins a table in memory (deprecated).					     |
 | DBCC proccache		|-- Displays information about procedure cache usage.			     |
 | DBCC show_statistics		|-- Displays statistics information for a table or indexed view.              |  
 | DBCC showcontig		|-- Displays fragmentation information for data/index pages.		     |
