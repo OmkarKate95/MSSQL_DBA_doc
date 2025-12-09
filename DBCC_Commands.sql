59ec5393f710aace98317a8f29bed3a3f7e66998
Omkar <127608288+OmkarKate95@users.noreply.github.com>
2025-12-09 22:15:51 +0530

    Update DBCC_Commands.sql

--- DBCC_Commands.sql
+++ DBCC_Commands.sql
@@ -19,23 +19,24 @@
 | DBCC help			|-- Lists all available DBCC commands or explains a specific one.	     |
 | DBCC indexdefrag		        |-- Defragments clustered and secondary indexes (deprecated).		     |
 | DBCC inputbuffer		        |-- Returns the last statement sent from a client to SQL Server.	             |
-| DBCC opentran			|-- Displays information about the oldest open transaction.	             |
-| DBCC outputbuffer		|-- Displays the output buffer of a specified SPID.			     |
-| DBCC pintable			|-- Pins a table in memory (deprecated).					     |
-| DBCC proccache		|-- Displays information about procedure cache usage.			     |
+| DBCC opentran			         |-- Displays information about the oldest open transaction.	             |
+| DBCC outputbuffer		        |-- Displays the output buffer of a specified SPID.			     |
+| DBCC pintable			        |-- Pins a table in memory (deprecated).					     |
+| DBCC proccache		        |-- Displays information about procedure cache usage.			     |
 | DBCC show_statistics		|-- Displays statistics information for a table or indexed view.              |  
-| DBCC showcontig		|-- Displays fragmentation information for data/index pages.		     |
+| DBCC showcontig		        |-- Displays fragmentation information for data/index pages.		     |
 | DBCC shrinkdatabase		|-- Shrinks the size of the data and log files in a database.	             | 
-| DBCC shrinkfile		|-- Shrinks a specified data or log file.				     |
-| DBCC sqlperf			|-- Reports on SQL Server performance statistics like log space.	             |
-| DBCC traceoff			|-- Disables a specified trace flag.	    				     |
-| DBCC traceon			|-- Enables a specified trace flag.					     |
+| DBCC shrinkfile		        |-- Shrinks a specified data or log file.				     |
+| DBCC sqlperf			        |-- Reports on SQL Server performance statistics like log space.	             |
+| DBCC traceoff			        |-- Disables a specified trace flag.	    				     |
+| DBCC traceon			        |-- Enables a specified trace flag.					     |
 | DBCC tracestatus		|-- Displays status of trace flags.					     |
 | DBCC tuplemover		|-- Internal use for columnstore indexes; manages tuple movements.	     |
 | DBCC unpintable		|-- Unpins a table from memory (deprecated).				     |
 | DBCC updateusage		|-- Reports and corrects page and row count inaccuracies.	             |
 | DBCC useroptions		|-- Returns SET options active for the current session.		             |
 | ----------------------------- | ----------------------------------------------------------------------------|
+
 
 
 
