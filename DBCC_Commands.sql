3731a7acfb50cf144d18dd313c9f3948c11601fc
Omkar <127608288+OmkarKate95@users.noreply.github.com>
2025-12-09 22:19:42 +0530

    Update DBCC_Commands.sql

--- DBCC_Commands.sql
+++ DBCC_Commands.sql
@@ -25,7 +25,7 @@
 | DBCC proccache		        |-- Displays information about procedure cache usage.			     |
 | DBCC show_statistics		|-- Displays statistics information for a table or indexed view.              |  
 | DBCC showcontig		        |-- Displays fragmentation information for data/index pages.		     |
-| DBCC shrinkdatabase		|-- Shrinks the size of the data and log files in a database.	             | 
+| DBCC shrinkdatabase		   |-- Shrinks the size of the data and log files in a database.	             | 
 | DBCC shrinkfile		        |-- Shrinks a specified data or log file.				     |
 | DBCC sqlperf			        |-- Reports on SQL Server performance statistics like log space.	             |
 | DBCC traceoff			        |-- Disables a specified trace flag.	    				     |
@@ -36,6 +36,7 @@
 | DBCC updateusage	    	 |-- Reports and corrects page and row count inaccuracies.	             |
 | DBCC useroptions		        |-- Returns SET options active for the current session.		             |
 | ----------------------------- | ----------------------------------------------------------------------------|
+
 
 
 
